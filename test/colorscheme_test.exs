6d49fa6ae494acbc3a1ab4ed8bcb89570a0d86ff
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-06-22 18:44:29 +0200

    Add Colorscheme.Terminal.to_plist/1

--- test/colorscheme_test.exs
+++ test/colorscheme_test.exs
@@ -1,8 +1,30 @@
-defmodule ColorschemeTest do
+defmodule Colorscheme.TerminalTest do
   use ExUnit.Case
-  doctest Colorscheme
+  doctest Colorscheme.Terminal
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "creates a color scheme for Terminal.app" do
+    assert Colorscheme.Terminal.to_plist(
+      black: "222222",
+      red: "FF0000",
+      green: "00FF00",
+      yellow: "FFFF00",
+      blue: "0000FF",
+      magenta: "FF00FF",
+      cyan: "00FFFF",
+      white: "AAAAAA",
+
+      bright_black: "888888",
+      bright_red: "FF8888",
+      bright_green: "00FF88",
+      bright_yellow: "FFFF88",
+      bright_blue: "8888FF",
+      bright_magenta: "FF88FF",
+      bright_cyan: "88FFFF",
+      bright_white: "DDDDDD",
+
+      background: "000000",
+      bold: "FFFFFF",
+      text: "FFFFFF"
+    ) == File.read!("test/example.terminal")
   end
 end
