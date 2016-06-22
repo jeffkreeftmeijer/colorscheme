584ba536567b1883209df86803c1e16b8677965d
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-06-23 00:28:31 +0200

    Add color_to_rgb/1

--- test/colorscheme_test.exs
+++ test/colorscheme_test.exs
@@ -32,6 +32,10 @@ defmodule Colorscheme.TerminalTest do
     assert Colorscheme.Terminal.color_to_binary_plist([1,0,1]) == "bplist00\xD4\x01\x02\x03\x04\x05\x06\x15\x16X$versionX$objectsY$archiverT$top\x12\x00\x01\x86\xA0\xA3\a\b\x0FU$null\xD3\t\n\v\f\r\x0EUNSRGB\\NSColorSpaceV$classO\x10'1 0 1\x00\x10\x01\x80\x02\xD2\x10\x11\x12\x13Z$classnameX$classesWNSColor\xA2\x12\x14XNSObject_\x10\x0FNSKeyedArchiver\xD1\x17\x18Troot\x80\x01\b\x11\x1A#-27;AHN[b\x8C\x8E\x90\x95\xA0\xA9\xB1\xB4\xBD\xCF\xD2\xD7\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\x00\x19\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xD9"
   end
 
+  test "converts a color to a red, green and blue value" do
+    assert Colorscheme.Terminal.color_to_rgb("BFBFBF") == [0.7490196078, 0.7490196078, 0.7490196078]
+  end
+
   test "converts a color to a bas64 encoded string" do
     assert Colorscheme.Terminal.color_to_base64_encoded_string("222222") == """
     YnBsaXN0MDDUAQIDBAUGFRZYJHZlcnNpb25YJG9iamVjdHNZJGFyY2hpdmVyVCR0b3AS
