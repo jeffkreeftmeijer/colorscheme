c0d4f18ad6a7b51565450fa2a682af6df8f01980
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-06-23 09:40:57 +0200

    Update example Terminal profile
    The previous profile had colors with round numbers (like "FFFFFF" ->
    1,1,1 or "00FF00" -> 0,1,0), which get converted to integers by
    Terminal.app's profile export.
    
    Converting the numbers to intergers in Colorscheme would add a lot of
    complexity, so we'll treat every color it gets as a Float instead.
    
    To be able to test this with a profile exported from Terminal.app, the
    test/example.terminal profile was updated so each color is a float (red,
    which was "FF0000" is now "FF0001", for example), ignoring the round
    numbers case.

--- test/colorscheme_test.exs
+++ test/colorscheme_test.exs
@@ -4,32 +4,33 @@ defmodule Colorscheme.TerminalTest do
 
   test "creates a color scheme for Terminal.app" do
     assert Colorscheme.Terminal.to_plist(
-      black: "222222",
-      red: "FF0000",
-      green: "00FF00",
-      yellow: "FFFF00",
-      blue: "0000FF",
-      magenta: "FF00FF",
-      cyan: "00FFFF",
-      white: "AAAAAA",
+      black: "222223",
+      red: "FF0001",
+      green: "00FF01",
+      yellow: "FFFF01",
+      blue: "0000FE",
+      magenta: "FF00FE",
+      cyan: "00FFFE",
+      white: "AAAAAB",
 
-      bright_black: "888888",
-      bright_red: "FF8888",
-      bright_green: "00FF88",
-      bright_yellow: "FFFF88",
-      bright_blue: "8888FF",
-      bright_magenta: "FF88FF",
-      bright_cyan: "88FFFF",
-      bright_white: "DDDDDD",
+      bright_black: "888889",
+      bright_red: "FF8889",
+      bright_green: "00FF89",
+      bright_yellow: "FFFF89",
+      bright_blue: "8888FE",
+      bright_magenta: "FF88FE",
+      bright_cyan: "88FFFE",
+      bright_white: "DDDDDE",
 
-      background: "000000",
-      bold: "FFFFFF",
-      text: "FFFFFF"
+      background: "000001",
+      bold: "FFFFFE",
+      text: "FFFFFE"
     ) == File.read!("test/example.terminal")
   end
 
   test "converts a color to a binary plist" do
-    assert Colorscheme.Terminal.color_to_binary_plist([1,0,1]) == "bplist00\xD4\x01\x02\x03\x04\x05\x06\x15\x16X$versionX$objectsY$archiverT$top\x12\x00\x01\x86\xA0\xA3\a\b\x0FU$null\xD3\t\n\v\f\r\x0EUNSRGB\\NSColorSpaceV$classO\x10'1 0 1\x00\x10\x01\x80\x02\xD2\x10\x11\x12\x13Z$classnameX$classesWNSColor\xA2\x12\x14XNSObject_\x10\x0FNSKeyedArchiver\xD1\x17\x18Troot\x80\x01\b\x11\x1A#-27;AHN[b\x8C\x8E\x90\x95\xA0\xA9\xB1\xB4\xBD\xCF\xD2\xD7\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\x00\x19\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xD9"
+    assert Colorscheme.Terminal.color_to_binary_plist([1.0,0.0,1.0]) == "bplist00\xD4\x01\x02\x03\x04\x05\x06\x15\x16X$versionX$objectsY$archiverT$top\x12\x00\x01\x86\xA0\xA3\a\b\x0FU$null\xD3\t\n\v\f\r\x0EUNSRGB\\NSColorSpaceV$classO\x10&1.0 0.0 1.0\x00\x10\x01\x80\x02\xD2\x10\x11\x12\x13Z$classnameX$classesWNSColor\xA2\x12\x14XNSObject_\x10\x0FNSKeyedArchiver\xD1\x17\x18Troot\x80\x01\b\x11\x1A#-27;AHN[b\x8B\x8D\x8F\x94\x9F\xA8\xB0\xB3\xBC\xCE\xD1\xD6\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\x00\x19\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xD8"
+    assert Colorscheme.Terminal.color_to_binary_plist([0.1333333333, 0.1333333333, 0.137254902]) == "bplist00\xD4\x01\x02\x03\x04\x05\x06\x15\x16X$versionX$objectsY$archiverT$top\x12\x00\x01\x86\xA0\xA3\a\b\x0FU$null\xD3\t\n\v\f\r\x0EUNSRGB\\NSColorSpaceV$classO\x10&0.1333333333 0.1333333333 0.137254902\x00\x10\x01\x80\x02\xD2\x10\x11\x12\x13Z$classnameX$classesWNSColor\xA2\x12\x14XNSObject_\x10\x0FNSKeyedArchiver\xD1\x17\x18Troot\x80\x01\b\x11\x1A#-27;AHN[b\x8B\x8D\x8F\x94\x9F\xA8\xB0\xB3\xBC\xCE\xD1\xD6\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\x00\x19\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xD8"
   end
 
   test "converts a color to a red, green and blue value" do
@@ -37,6 +38,7 @@ defmodule Colorscheme.TerminalTest do
   end
 
   test "converts a color to a bas64 encoded string" do
-    assert Colorscheme.Terminal.color_to_base64_encoded_string("222222") == "YnBsaXN0MDDUAQIDBAUGFRZYJHZlcnNpb25YJG9iamVjdHNZJGFyY2hpdmVyVCR0b3AS\n\tAAGGoKMHCA9VJG51bGzTCQoLDA0OVU5TUkdCXE5TQ29sb3JTcGFjZVYkY2xhc3NPECcw\n\tLjEzMzMzMzMzMzMgMC4xMzMzMzMzMzMzIDAuMTMzMzMzMzMzMwAQAYAC0hAREhNaJGNs\n\tYXNzbmFtZVgkY2xhc3Nlc1dOU0NvbG9yohIUWE5TT2JqZWN0XxAPTlNLZXllZEFyY2hp\n\tdmVy0RcYVHJvb3SAAQgRGiMtMjc7QUhOW2KMjpCVoKmxtL3P0tcAAAAAAAABAQAAAAAA\n\tAAAZAAAAAAAAAAAAAAAAAAAA2Q=="
+    assert Colorscheme.Terminal.color_to_base64_encoded_string("222222") == "YnBsaXN0MDDUAQIDBAUGFRZYJHZlcnNpb25YJG9iamVjdHNZJGFyY2hpdmVyVCR0b3AS\n\tAAGGoKMHCA9VJG51bGzTCQoLDA0OVU5TUkdCXE5TQ29sb3JTcGFjZVYkY2xhc3NPECYw\n\tLjEzMzMzMzMzMzMgMC4xMzMzMzMzMzMzIDAuMTMzMzMzMzMzMwAQAYAC0hAREhNaJGNs\n\tYXNzbmFtZVgkY2xhc3Nlc1dOU0NvbG9yohIUWE5TT2JqZWN0XxAPTlNLZXllZEFyY2hp\n\tdmVy0RcYVHJvb3SAAQgRGiMtMjc7QUhOW2KLjY+Un6iws7zO0dYAAAAAAAABAQAAAAAA\n\tAAAZAAAAAAAAAAAAAAAAAAAA2A=="
+    assert Colorscheme.Terminal.color_to_base64_encoded_string("222223") == "YnBsaXN0MDDUAQIDBAUGFRZYJHZlcnNpb25YJG9iamVjdHNZJGFyY2hpdmVyVCR0b3AS\n\tAAGGoKMHCA9VJG51bGzTCQoLDA0OVU5TUkdCXE5TQ29sb3JTcGFjZVYkY2xhc3NPECYw\n\tLjEzMzMzMzMzMzMgMC4xMzMzMzMzMzMzIDAuMTM3MjU0OTAyABABgALSEBESE1okY2xh\n\tc3NuYW1lWCRjbGFzc2VzV05TQ29sb3KiEhRYTlNPYmplY3RfEA9OU0tleWVkQXJjaGl2\n\tZXLRFxhUcm9vdIABCBEaIy0yNztBSE5bYouNj5SfqLCzvM7R1gAAAAAAAAEBAAAAAAAA\n\tABkAAAAAAAAAAAAAAAAAAADY"
   end
 end
