7e2fef5caacdfa5cac882f13f7cb340f1a9841da
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-06-25 12:22:53 +0200

    Add Colorscheme.Color.(red|green|blue)/1

--- lib/color.ex
+++ lib/color.ex
@@ -1,4 +1,19 @@
 defmodule Colorscheme.Color do
+  def red(color) do
+    [red, _, _] = color |> to_rgb
+    red
+  end
+
+  def green(color) do
+    [_, green, _] = color |> to_rgb
+    green
+  end
+
+  def blue(color) do
+    [_, _, blue] = color |> to_rgb
+    blue
+  end
+
   def to_rgb(color) do
     result = Regex.scan(~r/../, color) |> List.flatten
 
--- test/color_test.exs
+++ test/color_test.exs
@@ -2,6 +2,24 @@ defmodule Colorscheme.ColorTest do
   use ExUnit.Case
   doctest Colorscheme.Color
 
+  test "returns the red value for a color" do
+    assert Colorscheme.Color.red("FF0000") == 1
+    assert Colorscheme.Color.red("0F0000") == 0.058823529411764705
+    assert Colorscheme.Color.red("000000") == 0
+  end
+
+  test "returns the green value for a color" do
+    assert Colorscheme.Color.green("00FF00") == 1
+    assert Colorscheme.Color.green("000F00") == 0.058823529411764705
+    assert Colorscheme.Color.green("000000") == 0
+  end
+
+  test "returns the blue value for a color" do
+    assert Colorscheme.Color.blue("0000FF") == 1
+    assert Colorscheme.Color.blue("00000F") == 0.058823529411764705
+    assert Colorscheme.Color.blue("000000") == 0
+  end
+
   test "converts a color to a red, green and blue value" do
     assert Colorscheme.Color.to_rgb("BFBFBF") == [0.7490196078431373, 0.7490196078431373, 0.7490196078431373]
     assert Colorscheme.Color.to_rgb("A16946") == [0.6313725490196078, 0.4117647058823529, 0.27450980392156865]
