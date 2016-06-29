6061883e84f06a810c9d53207f526368bf714db6
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-06-29 08:44:14 +0200

    Support Elixir ~> 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Colorscheme.Mixfile do
   def project do
     [app: :colorscheme,
      version: "0.1.0",
-     elixir: "~> 1.3-rc",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
