21542a86a0df6779b0c6aa41c3b65deb815d1765
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-07-16 09:34:01 +0200

    Add Mix.Tasks.Generate

---
+++ lib/colorscheme/generate.ex
@@ -0,0 +1,19 @@
+defmodule Mix.Tasks.Generate do
+  use Mix.Task
+
+  def run(path) do
+    basename = Path.basename(path, ".exs")
+
+    Mix.Config.read!(path)
+    |> Enum.each(fn({key, colors}) ->
+      configuration = colors |> Colorscheme.Terminal.to_configuration
+      "#{basename}-#{key}.terminal" |> File.write!(configuration)
+
+      configuration = colors |> Colorscheme.Iterm2.to_configuration
+      "#{basename}-#{key}.itermcolors" |> File.write!(configuration)
+
+      configuration = colors |> Colorscheme.Vim.to_configuration
+      "#{basename}-#{key}.vim" |> File.write!(configuration)
+    end)
+  end
+end
